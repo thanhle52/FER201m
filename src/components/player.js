5dd84ddae7d9d32c15736e105f0d6bbe50d210bc
thanhle52 <thanhagency09@gmail.com>
2023-06-07 15:40:26 +0700

    init 10

--- src/components/player.js
+++ src/components/player.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/Main.css'
 import { Players } from '../Share/ListOfPlayers'
 export default function Player() {
+    const [player, setPlayer] = useState([]) 
     return (
         <div className='container'>
             {Players.map((player)=>(
@@ -10,10 +11,20 @@ export default function Player() {
                <img src={player.img}/>
                  <h3>{player.name}</h3>
                  <p className='title'>{player.club}</p>
-                 <p><button>Detail</button></p>
+                 <p><button onClick={()=>{setPlayer(player)}}><a href="#popup1" id="openPopUp">Detail</a></button></p>
                </div>
              </div>
             ))}
+            <div id="popup1" className="overlay">
+              <div className="popup">
+                <img src={player.img}/>
+                <h2>{player.name}</h2>
+                <a className="close" href="#">&times;</a>
+                <div className="content">
+                  {player.info}
+                </div>
+              </div>
+            </div>
         </div>
     )    
 }
